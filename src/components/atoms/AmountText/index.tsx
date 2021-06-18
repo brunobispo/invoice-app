d8929471a4cb20c657c99962a25ed3a453f374e6
Bruno Bispo <bruno.bispo@olxbr.com>
2021-06-18 14:44:44 -0300

    fix: render zero amounts with green

--- src/components/atoms/AmountText/index.tsx
+++ src/components/atoms/AmountText/index.tsx
@@ -5,7 +5,7 @@ import currency from "helpers/currency";
 type AmountTextProps = Pick<InvoiceType, "amount">;
 
 const AmountText = ({ amount }: AmountTextProps) => (
-  <Container isPositive={amount > 0}>{currency(amount)}</Container>
+  <Container isPositive={amount >= 0}>{currency(amount)}</Container>
 );
 
 export default AmountText;
